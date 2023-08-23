3e291dae78a720186d92b1ba1a98e331fed6b770
jeroenknol <jeroen-knol@live.nl>
2023-08-23 14:41:18 +0200

    feat: add initial box

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,14 +1,13 @@
 import { Canvas } from '@react-three/fiber';
+import { Box } from './Box';
 
 export const CanvasElement = () => {
   return (
     <Canvas>
       <ambientLight intensity={0.1} />
       <directionalLight color='red' position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+
+      <Box />
     </Canvas>
   );
 };
